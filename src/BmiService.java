b8f1d78cf859059b52b9d2943cf0a11dd7a24d1b
Anna Smirnova <anna@grraoo.ru>
2020-12-12 19:23:20 +0300

    Edited BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculate (int weight, double height) {
         double bmi = weight / (height * height);
-        System.out.println(bmi);
+        System.out.printf("%.1f\n", bmi);
         return bmi;
     }
 }
